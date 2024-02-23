c10ef307589ff5bfc128c0a7d3cf7ea4dea324dd
Austin1122-eng <austinlam1111@gmail.com>
2024-02-22 22:51:37 -0600

    git commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import ProfileForm from './components/ProfileForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ProfileForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
