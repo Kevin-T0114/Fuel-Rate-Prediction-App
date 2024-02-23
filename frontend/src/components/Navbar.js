07050b87b27aa6e85e84b44171721c1d349b47f3
Timothy-Lu-8 <lu.timothy.9@gmail.com>
2024-02-23 16:07:12 -0600

    Added home icon

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,15 @@
+import home from '../images/home icon 2.png'
+import Home from '../Home'
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
     return (
         <div className="navbar">
             <span className="material-symbols-outlined">account_circle</span>
-            <a href="#" className="profile">Profile</a>
+            <a href="/profile" className="profile">Profile</a>
             <a href="#" className="login">Login</a>
             <a href="#" className="register">Register</a>
+            <img src={home} className='home' onClick={() => window.open('/home', '_self')}></img>
         </div>
     )
 }
