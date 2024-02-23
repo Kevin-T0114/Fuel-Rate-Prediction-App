d51c342813ec45453c4bf3c9110851f86377c176
Austin1122-eng <austinlam1111@gmail.com>
2024-02-22 23:03:10 -0600

    git commit

--- frontend/src/components/ProfileForm.js
+++ frontend/src/components/ProfileForm.js
@@ -34,17 +34,17 @@ function ProfileForm({ userProfile, setUserProfile }) {
       <h1>Client Management</h1>
       <h2>Complete Your Profile</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Full Name" value={fullName} onChange={(e) => setFullName(e.target.value)} maxLength = {50} required />
-        <input type="text" placeholder="Address 1" value={address1} onChange={(e) => setAddress1(e.target.value)} maxLength = {100} required />
-        <input type="text" placeholder="Address 2" value={address2} onChange={(e) => setAddress2(e.target.value)} maxLength = {100} />
-        <input type="text" placeholder="City" value={city} onChange={(e) => setCity(e.target.value)} maxLength = {100} required />
+        <input type="text" placeholder="Full Name 50 Character Limit" value={fullName} onChange={(e) => setFullName(e.target.value)} maxLength = {50} required />
+        <input type="text" placeholder="Address 1 100 Character Limit" value={address1} onChange={(e) => setAddress1(e.target.value)} maxLength = {100} required />
+        <input type="text" placeholder="Address 2 100 Character Limit" value={address2} onChange={(e) => setAddress2(e.target.value)} maxLength = {100} />
+        <input type="text" placeholder="City 100 Character Limit" value={city} onChange={(e) => setCity(e.target.value)} maxLength = {100} required />
         <select value={state} placeholder = "State" onChange={(e) => setState(e.target.value)} required>
           <option value="">Select State</option>
           {usStates.map((state) => (
             <option key={state} value={state}>{state}</option>
           ))}
         </select>
-        <input type="text" placeholder="Zipcode" value={zipcode} onChange={(e) => setZipcode(e.target.value)} minLength = {5} maxLength = {9} required />
+        <input type="text" placeholder="Zipcode 5 Character Min 9 Character Max" value={zipcode} onChange={(e) => setZipcode(e.target.value)} minLength = {5} maxLength = {9} required />
         <button type="submit">Save</button>
       </form>
     </div>
