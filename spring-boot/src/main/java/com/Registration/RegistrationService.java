689ef8ea0d7b43d4a0bac52df1c130c19cb7c713
Kevin Tran <tran.kevin01142003@gmail.com>
2024-04-11 21:14:18 -0500

    added password encryption to DB

--- spring-boot/src/main/java/com/Registration/RegistrationService.java
+++ spring-boot/src/main/java/com/Registration/RegistrationService.java
@@ -1,6 +1,8 @@
 package com.Registration;
 
 import java.util.Optional;
+import java.security.NoSuchAlgorithmException;
+import java.security.MessageDigest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,11 +17,31 @@ public class RegistrationService { // figure out what to return
         this.registrationRepository = registrationRepository;
     }
 
+    private String encryptPassword(String password) {
+        String encryptedPassword = "";
+        try {
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.update(password.getBytes());
+            byte[] bytes = m.digest();
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            encryptedPassword = s.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return encryptedPassword;
+    }
+
     public Boolean addNewAccount(Registration registration) {
         Optional<Registration> reg = registrationRepository.findById(registration.getuserName());
         if (reg.isPresent()) {
             return false; // return false if username is already in database
         }
+        String encryptedPassword = encryptPassword(registration.getpassWord());
+        registration.setpassWord(encryptedPassword);
         registrationRepository.save(registration);
         return true; // return true if username is not in database
     }
