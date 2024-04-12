2efeea2d873741bf3a34b33b548d1dc2891cff56
Timothy-Lu-8 <lu.timothy.9@gmail.com>
2024-04-12 15:06:45 -0500

    update db.sql

--- spring-boot/src/test/java/com/FuelQuote/FuelQuoteTest.java
+++ spring-boot/src/test/java/com/FuelQuote/FuelQuoteTest.java
@@ -10,51 +10,51 @@ public class FuelQuoteTest {
     //constructor class variable tests
     @Test
     public void testID() {
-        FuelQuote obj = new FuelQuote(1, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
-        Assertions.assertEquals(1, obj.getID());
+        FuelQuote obj = new FuelQuote(1L, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
+        Assertions.assertEquals(1L, obj.getID());
     }
 
     @Test
     public void testGallonsRequested() {
-        FuelQuote obj = new FuelQuote(1, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
+        FuelQuote obj = new FuelQuote(1L, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
         Assertions.assertEquals(6, obj.getGallonsRequested());
     }
 
     @Test
     public void testAddress() {
-        FuelQuote obj = new FuelQuote(1, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
+        FuelQuote obj = new FuelQuote(1L, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
         Assertions.assertEquals("12345 Address Ln", obj.getAddress());
     }
 
     @Test
     public void testDeliveryDate() {
-        FuelQuote obj = new FuelQuote(1, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
+        FuelQuote obj = new FuelQuote(1L, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
         Assertions.assertEquals(LocalDate.of(2003, 6, 5), obj.getDeliveryDate());
     }
 
     @Test
     public void testPricePerGallon() {
-        FuelQuote obj = new FuelQuote(1, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
+        FuelQuote obj = new FuelQuote(1L, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
         Assertions.assertEquals(4, obj.getPricePerGallon());
     }
 
     @Test
     public void testTotalPrice() {
-        FuelQuote obj = new FuelQuote(1, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
+        FuelQuote obj = new FuelQuote(1L, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
         Assertions.assertEquals(24, obj.getTotalPrice());
     }
 
     //testing class member changes
     @Test
     public void testIDAfterChange() {
-        FuelQuote obj = new FuelQuote(1, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
-        obj.setID(5);
+        FuelQuote obj = new FuelQuote(1L, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
+        obj.setID(5L);
         Assertions.assertEquals(5, obj.getID());
     }
 
     @Test
     public void testTotalPriceAfterChangingGallonsRequested() {
-        FuelQuote obj = new FuelQuote(1, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
+        FuelQuote obj = new FuelQuote(1L, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
         obj.setGallonsRequested(13);
         obj.setTotalPrice(obj.getGallonsRequested(), obj.getPricePerGallon());
         Assertions.assertEquals(52, obj.getTotalPrice());
@@ -62,7 +62,7 @@ public class FuelQuoteTest {
 
     @Test
     public void testTotalPriceAfterChangingPricePerGallon() {
-        FuelQuote obj = new FuelQuote(1, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
+        FuelQuote obj = new FuelQuote(1L, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
         obj.setPricePerGallon(13.12);
         obj.setTotalPrice(obj.getGallonsRequested(), obj.getPricePerGallon());
         Assertions.assertEquals(78.72, obj.getTotalPrice());
@@ -70,14 +70,14 @@ public class FuelQuoteTest {
 
     @Test
     public void testAddressAfterChange() {
-        FuelQuote obj = new FuelQuote(1, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
+        FuelQuote obj = new FuelQuote(1L, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
         obj.setAddress("11111 Harlem Rd.");
         Assertions.assertEquals("11111 Harlem Rd.", obj.getAddress());
     }
 
     @Test
     public void testDeliveryDateAfterChange() {
-        FuelQuote obj = new FuelQuote(1, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
+        FuelQuote obj = new FuelQuote(1L, 6, "12345 Address Ln", LocalDate.of(2003, 6, 5), 4.0);
         obj.setDeliveryDate(LocalDate.of(2010, 5, 6));
         Assertions.assertEquals(LocalDate.of(2010, 5, 6), obj.getDeliveryDate());
     }
