6a5fce292d48c07365b07796981bb8edb5a16b93
Timothy-Lu-8 <lu.timothy.9@gmail.com>
2024-04-25 21:29:12 -0500

    fixed a test and order of fields displayed

--- frontend/src/components/Quote.js
+++ frontend/src/components/Quote.js
@@ -1,9 +1,9 @@
 const Quote = ({quote}) => {
     return (
     <tr>
-        <td>{quote.gallonsRequested}</td>
-        <td>{quote.address.toUpperCase()}</td>
         <td>{quote.deliveryDate}</td>
+        <td>{quote.address.toUpperCase()}</td>
+        <td>{quote.gallonsRequested}</td>
         <td>${quote.pricePerGallon}</td>
         <td>${quote.totalPrice}</td>
     </tr>
